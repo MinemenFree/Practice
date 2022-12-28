3e36d0e5b5ee97c71b86ef9455d0f3a68da0438d
Coccocoa's Helper <vicentecmvg@gmail.com>
2022-12-28 20:14:06 +0000

    Rebranding

--- src/main/java/rip/crystal/practice/essentials/command/management/AdminInformationCommand.java
+++ src/main/java/rip/crystal/practice/essentials/command/management/AdminInformationCommand.java
@@ -4,20 +4,19 @@ import org.bukkit.entity.Player;
 import rip.crystal.practice.api.command.BaseCommand;
 import rip.crystal.practice.api.command.Command;
 import rip.crystal.practice.api.command.CommandArgs;
-import rip.crystal.practice.cPractice;
+import rip.crystal.practice.Practice;
 import rip.crystal.practice.utilities.chat.CC;
 
 public class AdminInformationCommand extends BaseCommand {
 
-    @Command(name = "admin", aliases = {"admininformation"}, permission = "cpractice.owner")
+    @Command(name = "admin", aliases = {"admininformation"}, permission = "practice.admin")
     @Override
     public void onCommand(CommandArgs commandArgs) {
         Player player = commandArgs.getPlayer();
         player.sendMessage(CC.CHAT_BAR);
-        player.sendMessage(CC.translate(" &7▢ &9Plugin: &r" + "cPractice"));
-        player.sendMessage(CC.translate(" &7▢ &9Version: &r" + cPractice.get().getDescription().getVersion()));
-        player.sendMessage(CC.translate(" &7▢ &9License: &r" + cPractice.get().getMainConfig().getString("LICENSE")));
-        player.sendMessage(CC.translate(" &7▢ &9Developer: &r" + "ziue"));
+        player.sendMessage(CC.translate(" &7▢ &9Plugin: &r" + "Practice"));
+        player.sendMessage(CC.translate(" &7▢ &9Version: &r" + "1.0"));
+        player.sendMessage(CC.translate(" &7▢ &9Developer: &r" + "MinemenFree"));
         player.sendMessage(CC.CHAT_BAR);
     }
 }
