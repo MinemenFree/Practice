a5e5fcc6a133369658ccd3a03a56e31699b8c55f
Coccocoa's Helper <vicentecmvg@gmail.com>
2022-12-28 20:15:04 +0000

    Rebranding

--- src/main/java/rip/crystal/practice/essentials/command/management/SetSpawnCommand.java
+++ src/main/java/rip/crystal/practice/essentials/command/management/SetSpawnCommand.java
@@ -4,17 +4,17 @@ import org.bukkit.entity.Player;
 import rip.crystal.practice.api.command.BaseCommand;
 import rip.crystal.practice.api.command.Command;
 import rip.crystal.practice.api.command.CommandArgs;
-import rip.crystal.practice.cPractice;
+import rip.crystal.practice.Practice;
 import rip.crystal.practice.utilities.chat.CC;
 
 public class SetSpawnCommand extends BaseCommand {
 
-	@Command(name = "setspawn", permission = "cpractice.command.setspawn")
+	@Command(name = "setspawn", permission = "practice.command.setspawn")
 	@Override
 	public void onCommand(CommandArgs commandArgs) {
 		Player player = commandArgs.getPlayer();
 
-		cPractice.get().getEssentials().setSpawnAndSave(player.getLocation());
+		Practice.get().getEssentials().setSpawnAndSave(player.getLocation());
 		player.sendMessage(CC.GREEN + "You updated this world's spawn.");
 	}
 }
