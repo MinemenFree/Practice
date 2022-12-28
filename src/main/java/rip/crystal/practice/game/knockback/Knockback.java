d3c63d8ee7881939f6eced1617305a8b4871ed72
Coccocoa's Helper <vicentecmvg@gmail.com>
2022-12-28 22:04:08 +0000

    Update Knockback.java

--- src/main/java/rip/crystal/practice/game/knockback/Knockback.java
+++ src/main/java/rip/crystal/practice/game/knockback/Knockback.java
@@ -11,10 +11,6 @@ public class Knockback {
 
     public static void init() {
         switch (cPractice.get().getServer().getName()) {
-            case "GxSpigot":
-                knockbackProfiler = new GxSpigot();
-                System.out.print("Hooked into GxSpigot");
-                break;
             case "FoxSpigot":
                 knockbackProfiler = new FoxSpigot();
                 System.out.print("Hooked into FoxSpigot");
@@ -23,23 +19,13 @@ public class Knockback {
                 knockbackProfiler = new cSpigot();
                 System.out.print("Hooked into cSpigot");
                 break;
-            case "InsanePaper":
-                knockbackProfiler = new InsanePaperSpigot();
-                System.out.print("Hooked into InsanePaper");
-                break;
-            case "gSpigot":
-                knockbackProfiler = new gSpigot();
-                System.out.print("Hooked into gSpigot");
-                break;
             case "CarbonSpigot":
                 knockbackProfiler = new CarbonSpigot();
                 System.out.print("Hooked into CarbonSpigot");
                 break;
             default:
                 knockbackProfiler = new Default();
-                System.out.print("You don't have a spigot compatible with cpractice's Knockbacks");
-                System.out.print("You don't have a spigot compatible with cpractice's Knockbacks");
-                System.out.print("You don't have a spigot compatible with cpractice's Knockbacks");
+                System.out.print("You don't have a spigot compatible with cPractice's knockback-per-kit system");
                 break;
         }
     }
