521e516fb133dc7c84f19ead0c42c3c1e9329bcc
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-01-12 15:32:50 +0000

    Update Rank.java

--- src/main/java/rip/crystal/practice/api/rank/Rank.java
+++ src/main/java/rip/crystal/practice/api/rank/Rank.java
@@ -1,6 +1,11 @@
 package rip.crystal.practice.api.rank;
 
 import net.audidevelopment.core.api.player.GlobalPlayer;
+import net.audidevelopment.core.api.player.PlayerData;
+import net.audidevelopment.core.plugin.cCoreAPI;
+import me.activated.core.api.player.GlobalPlayer;
+import me.activated.core.api.player.PlayerData;
+import me.activated.core.plugin.AquaCoreAPI;
 import java.util.UUID;
 
 public interface Rank {
