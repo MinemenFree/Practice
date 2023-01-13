a34ef96973025e9074d393bc1e296400b3f51dbd
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-01-13 21:42:23 +0000

    Remove Shitcode

--- src/main/java/rip/crystal/practice/match/task/MatchLogicTask.java
+++ src/main/java/rip/crystal/practice/match/task/MatchLogicTask.java
@@ -39,7 +39,7 @@ public class MatchLogicTask extends BukkitRunnable {
 				match.setState(MatchState.PLAYING_ROUND);
 				match.sendMessage(Locale.MATCH_STARTED, new MessageFormat());
 				match.sendSound(Sound.ORB_PICKUP, 1.0F, 1.0F);
-				match.sendMessage(CC.translate(cPractice.get().getLang().getString("MATCH.WARNING")));
+				match.sendMessage(Locale.MATCH_WARNING, new MessageFormat());
 			} else {
 				match.sendMessage(Locale.MATCH_START_TIMER, new MessageFormat()
 					.add("{time}", String.valueOf(nextAction))
