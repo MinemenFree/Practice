117e4c65f22d2374a0927abde53b0ba7492a5ca0
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-01-25 20:13:45 +0000

    Update DuelProcedure.java

--- src/main/java/rip/crystal/practice/match/duel/DuelProcedure.java
+++ src/main/java/rip/crystal/practice/match/duel/DuelProcedure.java
@@ -48,9 +48,9 @@ public class DuelProcedure {
 		Profile targetProfile = Profile.get(target.getUniqueId());
 		targetProfile.getDuelRequests().add(duelRequest);
 		
-		PlayerUtil targetPing = PlayerUtil.get(getPing(target));
+		PlayerUtil targetPing = PlayerUtil.get(PlayerUtil.getPing(target));
 		
-		PlayerUtil senderPing = PlayerUtil.get(getPing(sender));
+		PlayerUtil senderPing = PlayerUtil.get(PlayerUtil.getPing(sender));
 
 		if (party) {
 			//sender.sendMessage(Locale.DUEL_SENT_PARTY.format(kit.getName(), target.getName(),
