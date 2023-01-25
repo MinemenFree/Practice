af0dafa22bf7115885b4254324fb23070e7ef8f3
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-01-25 14:55:55 +0000

    Update EventMapCreateCommand.java

--- src/main/java/rip/crystal/practice/game/event/game/map/command/EventMapCreateCommand.java
+++ src/main/java/rip/crystal/practice/game/event/game/map/command/EventMapCreateCommand.java
@@ -17,8 +17,8 @@ public class EventMapCreateCommand extends BaseCommand {
 	@Command(name = "event.map.create", permission = "cpractice.event.maps")
 	@Override
 	public void onCommand(CommandArgs commandArgs) {
-		Profile profile = Profile.get(player.getUniqueId());
 		Player player = commandArgs.getPlayer();
+		Profile profile = Profile.get(player.getUniqueId());
 		String[] args = commandArgs.getArgs();
 
 		if (args.length == 0 || args.length == 1) {
