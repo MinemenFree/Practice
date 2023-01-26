700a540aaa3eb0d92422fe76393ae8aa990e7bda
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-01-26 21:03:07 +0000

    GG

--- src/main/java/rip/crystal/practice/match/impl/BasicTeamMatch.java
+++ src/main/java/rip/crystal/practice/match/impl/BasicTeamMatch.java
@@ -384,7 +384,7 @@ public class BasicTeamMatch extends Match {
                                         }
 
 					if (kit.getGameRules().isBoxing()) {
-						//String mmcCombo = boxingCombo;
+						String mmcCombo = boxingCombo;
 						//String finalActualHits = actualHits;
 						config.getStringList("FIGHTS.1V1.BOXING-MODE").forEach(line -> {
 							lines.add(line.replace("{bars}", bars)
@@ -407,7 +407,7 @@ public class BasicTeamMatch extends Match {
 									.replace("<opponent-hits>", String.valueOf(opponent.getLeader().getHits()))
 									.replace("<your-combo>", String.valueOf(yours.getLeader().getCombo()))
 									.replace("<opponent-combo>", String.valueOf(opponent.getLeader().getCombo())));
-									//.replace("{combo}", (yours.getLeader().getCombo >= opponent.getLeader().getCombo ? CC.GREEN : CC.WHITE) + (yours.getLeader().getCombo <= opponent.getLeader().getCombo ? CC.RED : CC.WHITE) + (yours.getLeader().getCombo > opponent.getLeader().getCombo ? yours.getLeader().getCombo() : opponent.getLeader().getCombo() : "No") + "Combo"));
+									.replace("<combo>", mmcCombo);
 						});
 						return lines;
 					}
