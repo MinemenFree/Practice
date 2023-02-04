0b5d93e6e89649ef98ee68a01a20be1e93ea6cac
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-02-04 22:18:03 +0000

    Update DuelRequest.java

--- src/main/java/rip/crystal/practice/match/duel/DuelRequest.java
+++ src/main/java/rip/crystal/practice/match/duel/DuelRequest.java
@@ -43,9 +43,9 @@ public class DuelRequest {
 					.add("<kit_name>", kit.getName())
 				        .add("<arena_author>", arena.getAuthor())
 					.add("<target_name>", target.getName())
-				        //.add("<target_ping>", Integer.toString(PlayerUtil.getPing(target))
+				        .add("<target_ping>", Integer.toString(PlayerUtil.getPing(target)))
 					.add("<sender_name>", sender.getName())
-					//.add("<sender_ping>", Integer.toString(PlayerUtil.getPing(sender))
+					.add("<sender_ping>", Integer.toString(PlayerUtil.getPing(sender)))
 					.send(sender);
 
 			new MessageFormat(Locale.DUEL_TARGET_EXPIRED.format(Profile.get(target.getUniqueId()).getLocale()))
@@ -53,9 +53,9 @@ public class DuelRequest {
 					.add("<kit_name>", kit.getName())
 				        .add("<arena_author>", arena.getAuthor())
 					.add("<sender_name>", sender.getName())
-					//.add("<sender_ping>", Integer.toString(PlayerUtil.getPing(sender))
+					.add("<sender_ping>", Integer.toString(PlayerUtil.getPing(sender)))
 					.add("<target_name>", target.getName())
-					//.add("<target_ping>", Integer.toString(PlayerUtil.getPing(target))
+					.add("<target_ping>", Integer.toString(PlayerUtil.getPing(target)))
 					.send(target);
 		}
 	}
