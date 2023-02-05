83bfea92f7a840334aa81527b47239e505403746
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-02-05 00:31:26 +0000

    Update WindSpigot.java

--- src/main/java/rip/crystal/practice/game/knockback/impl/WindSpigot.java
+++ src/main/java/rip/crystal/practice/game/knockback/impl/WindSpigot.java
@@ -3,8 +3,8 @@ package rip.crystal.practice.game.knockback.impl;
 import net.minecraft.server.v1_8_R3.EntityHuman;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import dev.cobblesword.nachospigot.knockback.KnockbackConfig;
-import dev.cobblesword.nachospigot.knockback.KnockbackProfile;
+import ga.windpvp.windspigot.knockback.KnockbackConfig;
+import ga.windpvp.windspigot.knockback.CraftKnockbackProfile;
 import rip.crystal.practice.game.knockback.KnockbackProfiler;
 
 import java.lang.reflect.Field;
