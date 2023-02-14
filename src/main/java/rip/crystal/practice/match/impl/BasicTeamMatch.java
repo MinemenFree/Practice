0016f52f43f4466b924a32d925894c4e5b48b132
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-02-14 19:56:01 +0000

    yeah yeah

--- src/main/java/rip/crystal/practice/match/impl/BasicTeamMatch.java
+++ src/main/java/rip/crystal/practice/match/impl/BasicTeamMatch.java
@@ -578,9 +578,9 @@ public class BasicTeamMatch extends Match {
 
 		BaseComponent[] inventories = generateInventoriesComponents(
 			new MessageFormat(Locale.MATCH_END_DETAILS.format(profile.getLocale()))
-				.add("<winner>", winningParticipant)
+				.add("<winner>", winningParticipant.toString()))
 				.add("<winner-context>", participantA.getPlayers().size() == 1 ? "" : "s")
-				.add("<loser>", losingParticipant)
+				.add("<loser>", losingParticipant.toString())
 				.add("<loser-context>", participantB.getPlayers().size() > 1 ? "s" : ""));
 
 		if (participantA.getPlayers().size() == 1 && participantB.getPlayers().size() == 1) {
