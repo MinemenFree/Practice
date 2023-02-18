55441e4e514fcbc2aaf43b4d90c507f92c25937f
Coccocoa's Helper <vicentecmvg@gmail.com>
2023-02-18 00:02:12 +0000

    Update PracticeInfoCommand.java

--- src/main/java/rip/crystal/practice/essentials/command/management/PracticeInfoCommand.java
+++ src/main/java/rip/crystal/practice/essentials/command/management/PracticeInfoCommand.java
@@ -16,18 +16,16 @@ public class PracticeInfoCommand extends BaseCommand {
     @Override
     public void onCommand(CommandArgs commandArgs) {
         Player player = commandArgs.getPlayer();
-        player.sendMessage(CC.translate("     &9" + cPractice.get().getName()));
-        player.sendMessage(CC.translate(""));
-        player.sendMessage(CC.translate(" &7| &9Author&7: &f" + cPractice.get().getDescription().getAuthors().toString().replace("[", "").replace("]", "")));
-        player.sendMessage(CC.translate(" &7| &9Version&7: &f" + cPractice.get().getDescription().getVersion()));
-        player.sendMessage(CC.translate(" &7| &9License&7: &f" + cPractice.get().getMainConfig().getString("LICENSE")));
-        player.sendMessage(CC.translate(" &7| &9Rank System&7: &f" + cPractice.get().getRankManager().getRankSystem()));
-        player.sendMessage(CC.translate(" "));
-        player.sendMessage(CC.translate(" &7| &9Spigot&7: &f" + cPractice.get().getServer().getName()));
-        player.sendMessage(CC.translate(" "));
-        player.sendMessage(CC.translate(" &7| &9Arenas&7: &f" + Arena.getArenas().size()));
-        player.sendMessage(CC.translate(" &7| &9Kits&7: &f" + Kit.getKits().size()));
-        player.sendMessage(CC.translate(" &7| &9Ranked Kits&7: &f" + Kit.getKits().stream().filter(kit -> kit.getGameRules().isRanked()).count()));
-        player.sendMessage(CC.translate(" &7| &9Clans&7: &f" + Clan.getClans().size()));
+        new MessageFormat(Locale.PRACTICE_INFO.format(profile.getLocale()))
+                .add("<practice_name>", cPractice.get().getName())
+                .add("<practice_version>", cPractice.get().getDescription().getVersion())
+                .add("<practice_license>", cPractice.get().getMainConfig().getString("LICENSE"))
+                .add("<rank_core>", cPractice.get().getRankManager().getRankSystem())
+                .add("<spigot>", cPractice.get().getServer().getName())
+                .add("<arenas>", Arena.getArenas().size())
+                .add("<kits>", Kit.getKits().size())
+                .add("<ranked_kits>", Kit.getKits().stream().filter(kit -> kit.getGameRules().isRanked()).count())
+                .add("<clans>", Clan.getClans().size())
+				.send(player);
     }
 }
