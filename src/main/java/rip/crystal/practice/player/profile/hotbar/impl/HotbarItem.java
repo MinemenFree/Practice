73e7b3bf065d328ca2bca442f700f0915732606b
ziuee <remig06@hotmail.com>
2023-03-16 14:44:32 +0100

    Fixed `HotbarItem.java`

--- src/main/java/rip/crystal/practice/player/profile/hotbar/impl/HotbarItem.java
+++ src/main/java/rip/crystal/practice/player/profile/hotbar/impl/HotbarItem.java
@@ -10,45 +10,45 @@ import java.util.regex.Pattern;
 
 public enum HotbarItem {
 	
-	QUEUE_JOIN_RANKED(cPractice.get().getQueueConfig().getString("QUEUE_JOIN_RANKED.COMMAND")),
-	QUEUE_JOIN_UNRANKED(cPractice.get().getQueueConfig().getString("QUEUE_JOIN_UNRANKED.COMMAND")),
-	QUEUES_JOIN(cPractice.get().getQueueConfig().getString("QUEUES_JOIN.COMMAND")),
-	QUEUE_LEAVE(cPractice.get().getQueueConfig().getString("QUEUE_LEAVE.COMMAND")),
+	QUEUE_JOIN_RANKED(cPractice.get().getHotbarConfig().getString("QUEUE_JOIN_RANKED.COMMAND")),
+	QUEUE_JOIN_UNRANKED(cPractice.get().getHotbarConfig().getString("QUEUE_JOIN_UNRANKED.COMMAND")),
+	QUEUES_JOIN(cPractice.get().getHotbarConfig().getString("QUEUES_JOIN.COMMAND")),
+	QUEUE_LEAVE(cPractice.get().getHotbarConfig().getString("QUEUE_LEAVE.COMMAND")),
 	PARTY_EVENTS(null),
-	PARTY_CREATE(cPractice.get().getQueueConfig().getString("PARTY_CREATE.COMMAND")),
-	PARTY_DISBAND(cPractice.get().getQueueConfig().getString("PARTY_DISBAND.COMMAND")),
-	PARTY_LEAVE(cPractice.get().getQueueConfig().getString("PARTY_LEAVE.COMMAND")),
-	PARTY_INFORMATION(cPractice.get().getQueueConfig().getString("PARTY_INFORMATION.COMMAND")),
+	PARTY_CREATE(cPractice.get().getHotbarConfig().getString("PARTY_CREATE.COMMAND")),
+	PARTY_DISBAND(cPractice.get().getHotbarConfig().getString("PARTY_DISBAND.COMMAND")),
+	PARTY_LEAVE(cPractice.get().getHotbarConfig().getString("PARTY_LEAVE.COMMAND")),
+	PARTY_INFORMATION(cPractice.get().getHotbarConfig().getString("PARTY_INFORMATION.COMMAND")),
 	OTHER_PARTIES(null),
-	KIT_EDITOR(cPractice.get().getQueueConfig().getString("KIT_EDITOR.COMMAND")),
-	SPECTATE_STOP(cPractice.get().getQueueConfig().getString("SPECTATE_STOP.COMMAND")),
+	KIT_EDITOR(cPractice.get().getHotbarConfig().getString("KIT_EDITOR.COMMAND")),
+	SPECTATE_STOP(cPractice.get().getHotbarConfig().getString("SPECTATE_STOP.COMMAND")),
 	VIEW_INVENTORY(null),
-	EVENT_JOIN(cPractice.get().getQueueConfig().getString("EVENT_JOIN.COMMAND")),
-	EVENT_LEAVE(cPractice.get().getQueueConfig().getString("EVENT_LEAVE.COMMAND")),
+	EVENT_JOIN(cPractice.get().getHotbarConfig().getString("EVENT_JOIN.COMMAND")),
+	EVENT_LEAVE(cPractice.get().getHotbarConfig().getString("EVENT_LEAVE.COMMAND")),
 	MAP_SELECTION(null),
 
 	FOLLOW(null),
 
-	REMATCH_REQUEST(cPractice.get().getQueueConfig().getString("REMATCH_REQUEST.COMMAND")),
-	REMATCH_ACCEPT(cPractice.get().getQueueConfig().getString("REMATCH_ACCEPT.COMMAND")),
+	REMATCH_REQUEST(cPractice.get().getHotbarConfig().getString("REMATCH_REQUEST.COMMAND")),
+	REMATCH_ACCEPT(cPractice.get().getHotbarConfig().getString("REMATCH_ACCEPT.COMMAND")),
 
 	CLASS_SELECT(null),
 	KIT_SELECTION(null),
-	EVENT_SELECT(cPractice.get().getQueueConfig().getString("EVENT_SELECT.COMMAND")),
+	EVENT_SELECT(cPractice.get().getHotbarConfig().getString("EVENT_SELECT.COMMAND")),
 	LEADERBOARD_MENU(null),
 
 	RANDOM_TELEPORT(null),
 	HIDE_ALL_PLAYERS(null),
 	VIEW_INVENTORYSTAFF(null),
-	SETTINGS(cPractice.get().getQueueConfig().getString("SETTINGS.COMMAND")),
+	SETTINGS(cPractice.get().getHotbarConfig().getString("SETTINGS.COMMAND")),
 	//FFA_JOIN("ffa join"),
 	RESET(null),
-	ONLINE_STAFF(cPractice.get().getQueueConfig().getString("ONLINE_STAFF.COMMAND")),
-	COMPASS(cPractice.get().getQueueConfig().getString("COMPASS.COMMAND")),
+	ONLINE_STAFF(cPractice.get().getHotbarConfig().getString("ONLINE_STAFF.COMMAND")),
+	COMPASS(cPractice.get().getHotbarConfig().getString("COMPASS.COMMAND")),
 	FREEZE(null),
 
-	VIEWTOURNAMENT(cPractice.get().getQueueConfig().getString("VIEWTOURNAMENT.COMMAND")),
-	LEAVETOURNAMENT(cPractice.get().getQueueConfig().getString("LEAVETOURNAMENT.COMMAND"));
+	VIEWTOURNAMENT(cPractice.get().getHotbarConfig().getString("VIEWTOURNAMENT.COMMAND")),
+	LEAVETOURNAMENT(cPractice.get().getHotbarConfig().getString("LEAVETOURNAMENT.COMMAND"));
 
 	@Getter private final String command;
 	@Getter @Setter private Pattern pattern;
