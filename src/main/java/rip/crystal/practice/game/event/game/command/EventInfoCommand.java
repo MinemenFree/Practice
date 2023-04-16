34cf18c2c31435c39a269667bc15e653b72f677b
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-04-16 19:53:51 +0100

    Update EventInfoCommand.java

--- src/main/java/rip/crystal/practice/game/event/game/command/EventInfoCommand.java
+++ src/main/java/rip/crystal/practice/game/event/game/command/EventInfoCommand.java
@@ -6,7 +6,9 @@ import rip.crystal.practice.api.command.Command;
 import rip.crystal.practice.api.command.CommandArgs;
 import rip.crystal.practice.game.event.game.EventGame;
 import rip.crystal.practice.game.event.impl.sumo.SumoEvent;
+import rip.crystal.practice.player.profile.Profile;
 import rip.crystal.practice.utilities.chat.CC;
+import rip.crystal.practice.utilities.MessageFormat;
 
 public class EventInfoCommand extends BaseCommand {
 
@@ -16,12 +18,18 @@ public class EventInfoCommand extends BaseCommand {
 		Player player = commandArgs.getPlayer();
 
 		if (EventGame.getActiveGame() == null) {
-			player.sendMessage(CC.RED + "There is no active event.");
+			new MessageFormat(Locale.EVENT_NO_ACTIVE_EVENT.format(profile.getLocale()));
 			return;
 		}
 
 		EventGame game = EventGame.getActiveGame();
 
+		new MessageFormat(Locale.EVENT_INFO.format(profile.getLocale()))
+                	.add("<player_limit>", game.getMaximumPlayers())
+			.add("<event_type>", game.getEvent().getName())
+                	.add("<remaining_players>", game.getRemainingPlayers())
+                	.add("<game_state>", game.getGameState().getReadable())
+			.send(player);
 		player.sendMessage(CC.translate("&7(*) &c&lEvent &7(*)"));
 		player.sendMessage(CC.RED + "Player Limit: " + CC.WHITE + game.getMaximumPlayers());
 		player.sendMessage(CC.RED + "Event Type: " + CC.WHITE + game.getEvent().getName());
@@ -29,6 +37,12 @@ public class EventInfoCommand extends BaseCommand {
 		player.sendMessage(CC.RED + "State: " + CC.WHITE + game.getGameState().getReadable());
 
 		if (game.getEvent() instanceof SumoEvent) {
+			new MessageFormat(Locale.EVENT_SUMO_ROUND.format(profile.getLocale()))
+                		.add("<player_limit>", game.getMaximumPlayers())
+				.add("<event_type>", game.getEvent().getName())
+                		.add("<remaining_players>", game.getRemainingPlayers())
+                		.add("<game_state>", game.getGameState().getReadable())
+				.send(player);
 			player.sendMessage(CC.RED + "Round: " + CC.WHITE + game.getGameLogic().getRoundNumber());
 		}
 	}
