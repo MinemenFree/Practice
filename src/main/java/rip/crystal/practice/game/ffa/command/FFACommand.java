4971014f8f5f632871852e92dbaea37369ac2a75
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2023-06-07 21:54:32 +0000

    Actually making changes

--- src/main/java/rip/crystal/practice/game/ffa/command/FFACommand.java
+++ src/main/java/rip/crystal/practice/game/ffa/command/FFACommand.java
@@ -1,15 +1,13 @@
 package rip.crystal.practice.game.ffa.command;
-/* 
-   Made by cpractice Development Team
-   Created on 27.11.2021
-*/
 
 import org.bukkit.entity.Player;
+import rip.crystal.practice.Locale;
 import rip.crystal.practice.api.command.BaseCommand;
 import rip.crystal.practice.api.command.Command;
 import rip.crystal.practice.api.command.CommandArgs;
 import rip.crystal.practice.game.ffa.command.subcommands.FFAJoinCommand;
 import rip.crystal.practice.game.ffa.command.subcommands.FFALeaveCommand;
+import rip.crystal.practice.utilities.MessageFormat;
 import rip.crystal.practice.utilities.chat.CC;
 
 public class FFACommand extends BaseCommand {
@@ -25,8 +23,7 @@ public class FFACommand extends BaseCommand {
         Player player = commandArgs.getPlayer();
         String[] args = commandArgs.getArgs();
         if (args.length == 0) {
-            player.sendMessage(CC.translate(("&9/ffa leave &7- &fLeave FFA")));
-            player.sendMessage(CC.translate(("&9/ffa join &7- &fJoin FFA")));
+            new MessageFormat(Locale.FFA_COMMAND_ARGS.format(profile.getLocale()));
         }
     }
 }
